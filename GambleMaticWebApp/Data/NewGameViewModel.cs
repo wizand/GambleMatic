7048e08189ea215a0f5313dda57ac44b01e5ff6a
Petteri Loisko <petteri.loisko@gmail.com>
2024-06-14 00:33:50 +0300

    Improved points view

---
+++ GambleMaticWebApp/Data/NewGameViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace GambleMaticWebApp.Data
+{
+    public class NewGameViewModel
+    {
+        public DateTime Date { get; set; } = DateTime.UtcNow;
+        public string Home { get; set; }
+        public string Away { get; set; }
+
+        public override string ToString()
+        {
+            return Date.ToShortDateString() + " " + Home + " vs " + Away;
+        }
+    }
+}
